e5c7cbbadc669be9f89263a4e7c5b5468512633b
Neel Chauhan <neel@neelc.org>
2021-03-16 19:01:45 -0700

    Make IPv6 prefix more configurable

--- server/mpd5/ipv6-down.sh
+++ server/mpd5/ipv6-down.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 INTERFACE=$1
-INTERFACE_NUMBER=`echo $INTERFACE | cut -c 3-`
-PID=`cat /var/ppp-v6/$INTERFACE_NUMBER.pid`
+PID=`cat /var/ppp-v6/$INTERFACE.pid`
 
 if [ "$PROTO" == "inet6" ]
 then
 	kill $PID
-	rm /var/ppp-v6/$INTERFACE_NUMBER.*
+	rm /var/ppp-v6/$INTERFACE.*
 fi
--- server/mpd5/ipv6-up.sh
+++ server/mpd5/ipv6-up.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 
+# User configurable options
+# Note: PREFIX_LEN has to be a multiple of 4
+PREFIX="666:420:69"
+PREFIX_LEN=48
+
+# Beyond here is (normally) not user-configurable for non-shell hackers
+
+PREFIX=`printf '$PREFIX' | sed 's/:://'`
+
+if [ $(( $PREFIX_LEN % 16 )) == 0 ]
+then
+	PREFIX=`printf '$PREFIX:'`
+fi
+
 INTERFACE=$1
-INTERFACE_NUMBER=`echo $INTERFACE | cut -c 3-`
+INTERFACE_INT=`echo $INTERFACE | cut -c 3-`
+INTERFACE_ID=`printf '%x' $INTERFACE_INT`
 PROTO=$2
 
 if [ "$PROTO" == "inet6" ]
 then
-	SUBNET=666:0:0:$INTERFACE_NUMBER
+	SUBNET=$PREFIX$INTERFACE_ID
 	ifconfig $INTERFACE inet6 $SUBNET::1 prefixlen 64
-	echo "$INTERFACE:\\" >> /var/ppp-v6/$INTERFACE_NUMBER.conf
-	echo "	:addr=\"666:0:0:$INTERFACE_NUMBER::\":prefixlen#64:" >> /var/ppp-v6/$INTERFACE_NUMBER.conf
-	rtadvd -D -c /var/ppp-v6/$INTERFACE_NUMBER.conf -p /var/ppp-v6/$INTERFACE_NUMBER.pid $INTERFACE
+	echo "$INTERFACE:\\" >> /var/ppp-v6/$INTERFACE_ID.conf
+	echo "	:addr=\"$SUBNET::\":prefixlen#64:" >> /var/ppp-v6/$INTERFACE.conf
+	rtadvd -D -c /var/ppp-v6/$INTERFACE.conf -p /var/ppp-v6/$INTERFACE.pid $INTERFACE
 fi
